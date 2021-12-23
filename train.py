f55656f378c27466eb173ecfb065e3276be6c9be
Konstantin Strassheim <konstantin.strassheim@gmail.com>
2021-12-23 20:21:52 +0100

    sileced distilbert training and added determinism

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 
 random_seed = 1337
 from os import environ
@@ -15,18 +17,11 @@ np.random.seed(random_seed)
 import random
 random.seed(random_seed)
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
-
 # Read data
 data = pd.read_json('data/dataset_1.json')
-data["label_bin"] = np.where(data["label"] > 1, 1, 0)
-data["label_n"] = data["label"] - 1
+data["label_train"] = data["label"] - 1
 data["display_text"] = [d[1]['text'][d[1]['displayTextRangeStart']: d[1]['getDisplayTextRangeEnd']] for d in data[["text","displayTextRangeStart", "getDisplayTextRangeEnd"]].iterrows()]
 max_display_text_length = len(data.iloc[np.argmax(data['display_text'].to_numpy())]['display_text'])
-
-# X = data.display_text.to_list()
-# y = data.label.to_list()
 
 validation_split_ratio = 0.2
 
@@ -38,18 +33,11 @@ model = DistilBertForSequenceClassification.from_pretrained(model_name, num_labe
 # ----- 1. Preprocess data -----#
 # Preprocess data
 X = list(data["display_text"])
-y = list(data["label_n"])
+y = list(data["label_train"])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=validation_split_ratio, random_state=random_seed, shuffle=True)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=validation_split_ratio * len(X) / len(X_train), random_state=random_seed, shuffle=True)
 
-X_train_trans = tokenizer(X, truncation=True, padding=True, max_length=512)
-X_test_trans = tokenizer(X_test, truncation=True, padding=True, max_length=512)
-X_val_trans = tokenizer(X_val, truncation=True, padding=True, max_length=512)
-
-# X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
-X_train_tokenized = tokenizer(X_train, padding=True, truncation=True, max_length=512)
-X_val_tokenized = tokenizer(X_val, padding=True, truncation=True, max_length=512)
 
 # Create torch dataset
 class Dataset(torch.utils.data.Dataset):
@@ -66,8 +54,7 @@ class Dataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.encodings["input_ids"])
 
-train_dataset = Dataset(X_train_tokenized, y_train)
-val_dataset = Dataset(X_val_tokenized, y_val)
+
 
 # ----- 2. Fine-tune pretrained model -----#
 # Define Trainer parameters
@@ -80,9 +67,10 @@ def compute_metrics(pred, labels):
 
     return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1}
 
-# def compute_metrics_eval(p):
-#     pred, labels = p
-#     compute_metrics(pred, labels)
+# create datasets
+train_dataset = Dataset(tokenizer(X_train, truncation=True, padding=True, max_length=512), y_train)
+val_dataset = Dataset(tokenizer(X_val, truncation=True, padding=True, max_length=512), y_val)
+test_dataset = Dataset(tokenizer(X_test, padding=True, truncation=True, max_length=512), y_test)
 
 # Define Trainer
 args = TrainingArguments(
@@ -106,13 +94,8 @@ trainer = Trainer(
 # Train pre-trained model
 trainer.train()
 
-# ----- 3. Predict -----#
-# Create torch dataset
-test_dataset = Dataset(tokenizer(X_test, padding=True, truncation=True, max_length=512))
-#     # Make prediction
-raw_pred, _, _ = trainer.predict(test_dataset)
-
-# Preprocess raw predictions
-# y_pred = np.argmax(raw_pred, axis=1)
-m = compute_metrics(raw_pred, y_test)
-print(m)
+# Test
+metrics = trainer.evaluate(test_dataset, metric_key_prefix="")
+#     raw_pred, _, _ = trainer.predict(test_dataset)
+#     m = compute_metrics(raw_pred, y_test)
+print(metrics)
